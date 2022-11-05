a1cf6b99c9e7ceae8abfdcb5e402b962c5904da6
Moch. Apriyanto Mahaliyan <79881169+liyan-227@users.noreply.github.com>
2022-11-05 10:15:17 +0700

    Update main.js
    Signed-off-by: Moch. Apriyanto Mahaliyan <79881169+liyan-227@users.noreply.github.com>

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Do you want to play music in the background?',
+    title: 'mau pake musik sayang?',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
