bbfa9bce6e435086c6dac67112cfd68d6cd27357
Moch. Apriyanto Mahaliyan <79881169+liyan-227@users.noreply.github.com>
2022-11-25 21:40:03 +0700

    Update main.js
    Signed-off-by: Moch. Apriyanto Mahaliyan <79881169+liyan-227@users.noreply.github.com>

--- script/main.js
+++ script/main.js
@@ -119,64 +119,16 @@ const animationTimeline = () => {
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
+    .to(".idea-4 strong", 0.5, {
       scale: 1.2,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff",
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(
-      ".idea-5",
-      0.7, {
-        rotationX: 15,
-        rotationZ: -10,
-        skewY: "-5deg",
-        y: 50,
-        z: 10,
-        opacity: 0,
-      },
-      "+=1.5"
-    )
-    .to(
-      ".idea-5 span",
-      0.7, {
-        rotation: 90,
-        x: 8,
-      },
-      "+=1.4"
-    )
-    .to(
-      ".idea-5",
-      0.7, {
-        scale: 0.2,
-        opacity: 0,
-      },
-      "+=2"
-    )
-    .staggerFrom(
-      ".idea-6 span",
-      0.8, {
-        scale: 3,
-        opacity: 0,
-        rotation: 15,
-        ease: Expo.easeOut,
-      },
-      0.2
-    )
-    .staggerTo(
-      ".idea-6 span",
-      0.8, {
-        scale: 3,
-        opacity: 0,
-        rotation: -15,
-        ease: Expo.easeOut,
-      },
-      0.2,
-      "+=1.5"
-    )
+    
     .staggerFromTo(
       ".baloons img",
       2.5, {
